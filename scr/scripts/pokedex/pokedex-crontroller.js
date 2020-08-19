1dd2838d3f7d0d55eb7c2db4ee862ab6db78ce66
prachurya deepta <deeptoadhikary@gmail.com>
2020-08-18 21:45:43 -0400

    qol changes

--- scr/scripts/pokedex/pokedex-crontroller.js
+++ scr/scripts/pokedex/pokedex-crontroller.js
@@ -6,6 +6,12 @@ export function PokedexController() {
         let query = getQueryString().toLowerCase();
         getPokemon(query).then(displayPokemon);
     });
+    setQueryEventListener((e) => {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            $("#search").click();
+        }
+    });
     getPokemon("charizard").then(displayPokemon);
 }
 
@@ -15,4 +21,8 @@ function setSearchEventListener(eventFn) {
 
 function getQueryString() {
     return $("#query").val();
+}
+
+function setQueryEventListener(eventFn) {
+    $("#query").keyup(eventFn);
 }
